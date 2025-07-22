d98c921bb76a2bbe13c5c64f63a70ec4cc087612
Rakshi2609 <rakshithganjimut@gmail.com>
2025-07-22 18:40:39 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandignNew from './componets/LandingCompo/LandingNew';
-import About from './componets/About/About';
-import Gallery from './componets/Gallery/Gallery';
-import NavBar from './componets/NavBar/NavBar';
-import SidePanel from './componets/SidePanel/SidePanel'; 
+import LandignNew from './components/LandingCompo/LandingNew';
+import About from './components/About/About';
+import Gallery from './components/Gallery/Gallery';
+import NavBar from './components/NavBar/NavBar';
+import SidePanel from './components/SidePanel/SidePanel'; 
 
 const App = () => {
   const [menuOpen, setMenuOpen] = useState(false);
